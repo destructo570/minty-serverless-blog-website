bf38ec342b8bb7f8ac27225800f150c6460fea59
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-03-14 23:19:38 +0530

    refac: refactor code

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <p>Signup</p>
+    <div className="grid sm:grid-cols-2">
+      <div className="hidden sm:block">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          beatae sunt asperiores, minima ipsum pariatur consequuntur accusamus.
+          Dignissimos tempore culpa illo vero, nesciunt labore. Qui,
+          exercitationem dolorem. Ad, sit rerum.
+        </p>
+      </div>
+      <div>
+        <h3>Signup/Login</h3>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
